cd363f672041d3eebabacd0a32249a46a1784eb9
daksear <daksear@gmail.com>
2025-07-10 01:17:00 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,4 +98,4 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Inherit the proprietary files
-include vendor/samsung/r6q/BoardConfigVendor.mk
+include vendor/samsung/r5q/BoardConfigVendor.mk
